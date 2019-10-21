a64090cda310aa7064ee8f6b63258459e9127fbb
pengzhe <zhe.peng@alibaba-inc.com>
2019-10-21 14:07:11 +0800

    test class

--- src/main/java/com/javaman/reflect/ClassObject.java
+++ src/main/java/com/javaman/reflect/ClassObject.java
@@ -1,5 +1,7 @@
 package com.javaman.reflect;
 
+import java.util.stream.Stream;
+
 /**
  * @author pengzhe
  * @date 2018/4/25 16:20
@@ -8,9 +10,22 @@ package com.javaman.reflect;
 
 public class ClassObject {
 
-    public static void main(String[] args) {
-        Object object = new Object();
+    public static void main(String[] args) throws ClassNotFoundException {
+       /* Object object = new Object();
         System.out.println("Object的全限定类名:" + object.getClass().getName());
-        System.out.println("===" + ClassObject.class.getName());
+        System.out.println("===" + ClassObject.class.getName());*/
+
+        TestClass aClass = new TestClass();
+        Stream.of(aClass.getClass().getDeclaredMethods())
+            .forEach(method -> System.out.println(method.getName()));
+
+        System.out.println("--------->" + aClass.getClass().toGenericString());
+
+        Class<?> forNameClass = Class.forName("com.javaman.reflect.TestClass");
+        Class<? extends TestClass> aClass1 = aClass.getClass();
+        Class<TestClass> testClassClass = TestClass.class;
+
+        System.out.println(forNameClass == aClass1);
+        System.out.println(aClass1 == testClassClass);
     }
 }
---
+++ src/main/java/com/javaman/reflect/TestClass.java
@@ -0,0 +1,16 @@
+package com.javaman.reflect;
+
+import lombok.Data;
+
+/**
+ * @author pengzhe
+ * @date 2019-10-21 13:59
+ */
+@Data
+public class TestClass {
+
+    private int id;
+    private String name;
+
+
+}
