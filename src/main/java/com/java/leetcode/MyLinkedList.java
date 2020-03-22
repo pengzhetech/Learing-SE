7fa4e7fc74382ca6379809a2695360c3a11b319a
pengzhe <pengzhepost@sina.com>
2020-03-22 23:24:17 +0800

    custom linklist

---
+++ src/main/java/com/java/leetcode/MyLinkedList.java
@@ -0,0 +1,164 @@
+package com.java.leetcode;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author pengzhe
+ * @date 2020/3/22 22:33
+ * @description
+ */
+
+public class MyLinkedList<AnyType> implements Iterable<AnyType> {
+
+    private int theSize;
+    private int modCount;
+    private Node<AnyType> beginMarker;
+    private Node<AnyType> endMarker;
+
+    public MyLinkedList() {
+        doClear();
+    }
+
+    private int size() {
+        return theSize;
+    }
+
+    private boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean add(AnyType x) {
+        add(size(), x);
+        return true;
+    }
+
+    public void add(int idx, AnyType x) {
+        addBefore(getNode(idx, 0, size()), x);
+    }
+
+    public AnyType get(int idx) {
+        return getNode(idx).data;
+
+    }
+
+    public AnyType set(int idx, AnyType newVal) {
+        Node<AnyType> p = getNode(idx);
+        AnyType oldVal = p.data;
+        p.data = newVal;
+        return oldVal;
+    }
+
+    private Node<AnyType> getNode(int idx) {
+        return getNode(idx, 0, size() - 1);
+    }
+
+    public AnyType remove(int idx) {
+        return remove(getNode(idx));
+    }
+
+    private AnyType remove(Node<AnyType> p) {
+        p.next.prev = p.prev;
+        p.prev.next = p.next;
+        theSize--;
+        modCount++;
+        return p.data;
+    }
+
+    private Node<AnyType> getNode(int idx, int lower, int upper) {
+        Node<AnyType> p;
+        if (idx < lower || idx > upper) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (idx < size() / 2) {
+            p = beginMarker.next;
+            for (int i = 0; i < idx; i++) {
+                p = p.next;
+            }
+        } else {
+            p = endMarker;
+            for (int i = size(); i > idx; i--) {
+                p = p.prev;
+            }
+        }
+        return p;
+    }
+
+    private void addBefore(Node<AnyType> p, AnyType x) {
+        Node<AnyType> newNode = new Node<>(x, p.prev, p);
+        newNode.prev.next = newNode;
+        p.prev = newNode;
+        theSize++;
+        modCount++;
+    }
+
+    private void clear() {
+        doClear();
+    }
+
+    private void doClear() {
+        beginMarker = new Node<>(null, null, null);
+        endMarker = new Node<>(null, beginMarker, null);
+        beginMarker.next = endMarker;
+    }
+
+
+    private static class Node<AnyType> {
+
+        public Node(AnyType d, Node<AnyType> p, Node<AnyType> n) {
+            data = d;
+            prev = p;
+            next = n;
+        }
+
+        public AnyType data;
+        public Node<AnyType> prev;
+        public Node<AnyType> next;
+    }
+
+
+    @Override
+    public Iterator<AnyType> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator<AnyType> {
+
+        private Node<AnyType> current = beginMarker.next;
+        private int expectModCount = modCount;
+        private boolean okToRemove = false;
+
+        @Override
+        public boolean hasNext() {
+            return current != endMarker;
+        }
+
+        @Override
+        public AnyType next() {
+            if (modCount != expectModCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            AnyType nextItem = current.data;
+            current = current.next;
+            okToRemove = true;
+            return nextItem;
+        }
+
+        public void remove() {
+            if (modCount != expectModCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (!okToRemove) {
+                throw new IllegalStateException();
+            }
+            MyLinkedList.this.remove(current.prev);
+            expectModCount++;
+            okToRemove = false;
+        }
+    }
+
+}
