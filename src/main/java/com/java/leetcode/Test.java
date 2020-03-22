c8839d1288695df3d311a533c273398a040a7420
pengzhe <pengzhepost@sina.com>
2020-03-22 22:29:30 +0800

    core java

---
+++ src/main/java/com/java/leetcode/Test.java
@@ -0,0 +1,13 @@
+package com.java.leetcode;
+
+/**
+ * @author pengzhe
+ * @date 2020/3/22 22:28
+ * @description
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
