31f31b08e7e76a4ad80e986e11e5d4a981ebb96b
pengzhe <pengzhepost@gmail.com>
2020-03-31 11:21:07 +0800

    problem 9

---
+++ src/main/java/com/java/data/problem9/Solution1.java
@@ -0,0 +1,16 @@
+package com.java.data.problem9;
+
+public class Solution1 {
+    public static boolean isPalindrome(int x) {
+        String reversedStr = (new StringBuilder(x + "")).reverse().toString();
+        return (x + "").equals(reversedStr);
+    }
+
+    public static void main(String[] args) {
+        int test = 11111;
+        int testint = 234;
+        System.out.println(isPalindrome(test));
+        System.out.println(isPalindrome(testint));
+    }
+
+}
---
+++ src/main/java/com/java/data/problem9/Solution2.java
@@ -0,0 +1,24 @@
+package com.java.data.problem9;
+
+public class Solution2 {
+    public boolean isPalindrome(int x) {
+        //边界判断
+        if (x < 0) {
+            return false;
+        }
+        int div = 1;
+        while (x / div >= 10) {
+            div *= 10;
+        }
+        while (x > 0) {
+            int left = x / div;
+            int right = x % 10;
+            if (left != right) {
+                return false;
+            }
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
+    }
+}
