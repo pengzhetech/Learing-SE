80bc3b42e55756da3dda77dcc3f5b7a6018ba9b0
pengzhe <pengzhepost@gmail.com>
2020-04-01 17:29:52 +0800

    problem 50

---
+++ src/main/java/com/java/data/problem50/Solution.java
@@ -0,0 +1,17 @@
+package com.java.data.problem50;
+
+class Solution {
+    public static double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        for (long i = 0; i < N; i++) {
+            ans = ans * x;
+        }
+        return ans;
+    }
+}
+
