ba50959534ffed97fbf1f9231234bf464e9c3a89
pengzhe <pengzhe@shizhuang-inc.com>
2024-02-12 19:08:03 +0800

    test

--- src/main/java/com/excel/Excel.java
+++ src/main/java/com/excel/Excel.java
@@ -1,10 +1,14 @@
 package com.excel;
 
-import com.google.common.collect.Lists;
-
-import java.util.List;
 
 public class Excel {
+
     public static void main(String[] args) {
+        String input = "23453d4534"; // 可以替换成任意字符串进行测试
+        if (input.matches("\\p{Nd}+")) {
+            System.out.println("字符串只包含纯阿拉伯数字");
+        } else {
+            System.out.println("字符串不只包含纯阿拉伯数字");
+        }
     }
 }
